079f9e4bfac66f7c5084cf638bcce3d4eed12af5
Jeremy Montes <jeremymontes@pursuit.org>
2023-04-27 16:00:56 -0400

    Edited Calculator project

--- index.js
+++ index.js
@@ -4,7 +4,51 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+console.log(process.argv)
+let sum;
+let operations = process.argv[2];
+let emptyArr = []
+let indexOfInfo = process.argv
+if(indexOfInfo.length <= 2){
+    return `No operation provided...`
+}
+else if(indexOfInfo.length <= 3){
+    return `No numbers provided...`
+}
+
+else if(operations === "plus"){
+    for(let i = 3;i<indexOfInfo.length;i++){
+        let num = indexOfInfo[i]
+        emptyArr.push(num)
+       sum = emptyArr.reduce((prev,curr)=>Number(prev) + Number(curr))
+    }
+    return sum
+}
+else if(operations === "minus"){
+    for(let i = 3;i<indexOfInfo.length;i++){
+        let num = indexOfInfo[i]
+        emptyArr.push(num)
+       sum = emptyArr.reduce((prev,curr)=>Number(prev) - Number(curr))
+    }
+    return sum
+}
+else if(operations !== "plus" || "minus"){
+    return `Invalid operation: ${operations}`
+}
+}
+// if(process.argv.length <= 2 ){
+//     return `No operation provided...`    
+// }
+// if(process.argv.length <= 3){
+//     return `No numbers provided...`
+// }
+// if(operations !== "plus" || "minus"){
+//     return `Invalid operation: ${operations}`
+// }
+
+calculator()
+
 
 // Don't change anything below this line.
 module.exports = calculator;
