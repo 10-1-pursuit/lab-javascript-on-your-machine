371795457812ecfa29cfb8b5d2c7e35554675445
xavierrice <xavier.rice@gmail.com>
2023-04-28 13:44:41 -0400

    still not passing--i don't know why my if statement isnt working on line 15

--- index.js
+++ index.js
@@ -4,7 +4,38 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+function calculator() {
+let sum;
+let operator = process.argv[2];
+const argumentInfo = process.argv
 
-// Don't change anything below this line.
+if(process.argv.length <= 2){
+    return "No operation provided...";
+};
+if(operator !== "plus" || operator !==  "minus"){
+    return `Invalid operation: ${operator}`
+};
+if(process.argv.length = 3){
+    return "No numbers provided..."
+};
+if (operator === "plus"){
+    for(let i = 3; i < argumentInfo.length; i++){  un
+    let num = Number(argumentInfo[i]);
+            sum += num         
+    };
+};
+if (operator === "minus"){
+    for(let i = 3; i < argumentInfo.length; i++){
+    let num = Number(argumentInfo[i]);
+            sum -= num         
+    };
+}; 
+
+return sum;
+
+}
+
+console.log(calculator())
+
+// Don't change anything blow this line.
 module.exports = calculator;
