0ba584086e159ab37910e350f9f5b0f5f305fc85
ShakaPursuit <shakalivngstone@pursuit.org>
2023-04-30 10:30:46 -0400

    Here

--- index.js
+++ index.js
@@ -4,51 +4,75 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator(plus,minus) {
-//console.log(process)
+function calculator(plus,minus,modulo,divide) {
+//console.log(process.argv)
 
-     add2Num = parseInt(`${process.argv[3]}`) +parseInt(`${process.argv[4]}`);
-         if(process.argv[2]=== plus){
-            return add2Num
+    
+   add2Num=  parseInt(`${process.argv[3]}`) +parseInt(`${process.argv[4]}`);
+     
+     if(process.argv[2]=== "plus" && process.argv.length < parseInt(5)){
+     
+                return add2Num;
          }
         
- console.log(`Adding two numbers: ${add2Num}`)
+ console.log(`Adding two numbers:${add2Num}`)
 
+        
+       
 
+ }
 
+         
     addMultipleNum = parseInt(`${process.argv[3]}`) + parseInt(`${process.argv[4]}`)+
  parseInt(`${process.argv[5]}`) + parseInt(`${process.argv[6]}`)+ parseInt(`${process.argv[7]}`);
-         if(process.argv[2] === plus){
-            return addMultipleNum
+        if(process.argv[2] === "plus" && process.argv.length === parseInt(7)){
+                return addMultipleNum;
          }
 
-console.log(`Adding multiple numbers:${addMultipleNum}`)
+//console.log(`Adding multiple numbers: ${addMultipleNum}`)
 
-        
+       
 
-    sub2Num= parseInt(`${process.argv[4]}`)- parseInt(`${process.argv[3]}`);
-        if(process.argv[2] === minus){
-            return sub2Num
+       
+            
+    sub2Num= parseInt(`${process.argv[3]}`)- parseInt(`${process.argv[4]}`);
+    if(process.argv[2] === "minus" && process.argv.length < parseInt(5)){
+            
+                return sub2Num;
         }
-console.log(`Subtracting two numbers : ${sub2Num}`)
+//console.log(`Subtracting two numbers : ${sub2Num}`)
         
         
+      
+        
+        
+    subMultipleNum = parseInt(`${process.argv[3]}`) - parseInt(`${process.argv[4]}`) -
+parseInt(`${process.argv[5]}`) - parseInt(`${process.argv[6]}`) - parseInt(`${process.argv[7]}`);
+       
+        if(process.argv[2] === "minus" && process.argv.length === parseInt(7)){
 
-    subMultipleNum=parseInt(`${process.argv[3]}`) -parseInt(`${process.argv[4]}`)-
-parseInt(`${process.argv[5]}`) - parseInt(`${process.argv[6]}`)- parseInt(`${process.argv[7]}`);
-        if(process.argv[2] === minus){
-            return subMultipleNum
+                return subMultipleNum
         }
-console.log(`Subtracting multiplenumbers : ${subMultipleNum}`)
+//console.log(`Subtracting multiplenumbers : ${subMultipleNum}`)
 
+         
+
+           
+            
+            
+            
     div2Num= parseInt(`${process.argv[3]}`)/parseInt(`${process.argv[4]}`);
-         if(process.argv[2] !== plus || process.argv[2] !== minus){
-             console.log( `Invalid operation: ${process.argv[2]}` );
-         }
+    if(process.argv[2] === "modulo"|| process.argv[2] === "divide"){
+         
+             console.log( `Invalid operation: Nan, ${process.argv[2]}` );
+               
+            
+
+                    }
+         
+                
         
-
-
-}
+            
 calculator()
 
 
