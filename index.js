75072a3ba48a969a691624ece33d091d45627aec
BrianYoung1223 <brianyoung@pursuit.org>
2023-04-30 21:15:52 -0400

    I tried my best on this rainy Sunday i been working on this lab all day, couldnt crack it

--- index.js
+++ index.js
@@ -4,7 +4,45 @@
  * Depending on the operation, either add up all of the numbers or subtract all of the numbers, from left to right.
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
-function calculator() {}
+
+
+
+
+
+
+
+
+function calculator() {
+     theOperator = process.argv[2];
+    if (theOperator !== ("plus" || "minus")){
+        return (`Invalid operation: ${theOperator}`)
+    }
+}
+
+   // console.log (process.argv[2])
+   if (firstStep !== "plus" || firstStep !== "minus"){
+        return `Invalid operation: ${process.argv[2]}`
+   }
+    let firstStep = process.argv[2]
+   if (firstStep === undefined){
+        return "No operation provided..."
+    }
+    if (firstStep !== "plus" || firstStep !== "minus"){
+     return `Invalid operation: ${process.argv[2]}`
+}
+    let numbers = process.argv.slice(3)
+       if (numbers.length === 0){
+            return "No numbers provided..."
+       }
+       if (typeof Number (numbers[0]) !== `number`){
+            return "No numbers provided..."
+        }
+    
+        console.log (process.argv[2]);
+
+//console.log (calculator( parseInt(`${process.argv[3]}`) + parseInt(`${process.argv[4]}`)));
+   // console.log(process.argv[2])
 
 // Don't change anything below this line.
 module.exports = calculator;
+
