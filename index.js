fbe3751f2cf441de4824cbaed0457e3c44023404
Christine Marchese <chm978@gmail.com>
2023-05-01 08:39:44 -0400

    Higher Order Functions lab

--- index.js
+++ index.js
@@ -5,32 +5,60 @@
  * @returns {number} The result of either adding all numbers or subtracting all numbers, depending on the arguments added to the command line.
  */
 function calculator() {
-    console.log(process.argv)
+    console.log(process.argv);
     let operation = process.argv[2];
-    if (operation === undefined) {
-        return 'No operation provided...';
+
+    if (!operation) {
+        return "No operation provided..."
     }
+    // let number = Number(process.argv[2]);
 
-    let numbers = process.argv.slice(3);
-
-    if (numbers.length === 0) {
-        return 'No numbers provided...'
+    if (process.argv.length <= 3) {
+        return "No numbers provided..."
+    } 
+    if(process.argv[2] !== 'plus' && process.argv[2] !== 'minus'){
+        return `Invalid operation: ${process.argv[2]}`
     }
+    let number = Number(process.argv[3]);
 
-    if (operation !== 'plus' && operation !== 'minus') {
-        return `Invalid operation: ${operation}`
-    }
-    let numbers1 = Number(process.argv[3]);
-
-    for (let i = 4; i < process.argv.length; i++) {
-        if (process.argv[2] === 'plus') {
-            numbers1 += Number(process.argv[i])
+    for(let i = 4; i < process.argv.length; i++){
+        if(process.argv[2] === 'plus'){
+            number += Number(process.argv[i])
         }
-        if (process.argv[2] === 'minus') {
-            numbers1 -= Number(process.argv[i]);
+        if(process.argv[2] === 'minus'){
+            number -= Number(process.argv[i])
         }
     }
-    return numbers1
+
+   return number
+
+
+    //     console.log(process.argv)
+    //     let operation = process.argv[2];
+    //     if (!operation) {
+    //         return 'No operation provided...';
+    //     }
+
+    //     let numbers = process.argv.slice(3);
+
+    //     if (numbers.length === 0) {
+    //         return 'No numbers provided...'
+    //     }
+
+    //     if (operation !== 'plus' && operation !== 'minus') {
+    //         return `Invalid operation: ${operation}`
+    //     }
+    //     let numbers1 = Number(process.argv[3]);
+
+    //     for (let i = 4; i < process.argv.length; i++) {
+    //         if (process.argv[2] === 'plus') {
+    //             numbers1 += Number(process.argv[i])
+    //         }
+    //         if (process.argv[2] === 'minus') {
+    //             numbers1 -= Number(process.argv[i]);
+    //         }
+    //     }
+    //     return numbers1
 }
 calculator();
 // Don't change anything below this line.
